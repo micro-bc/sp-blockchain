04e90c6ddf2f70bfe2c6cb1f4cd271b617cb446c
Urban Prah <urban.prah@student.um.si>
2020-04-19 00:10:30 +0200

    Implemented isValid(chain) [Blockchain.js]

--- blockchain/Blockchain.js
+++ blockchain/Blockchain.js
@@ -5,4 +5,23 @@ const Block = require("./Block.js");
  */
 var chain = new Array(Block.genesisBlock);
 
-module.exports = chain;
+/**
+ * isValid()
+ * @description checks block validity for all blocks in the chain
+ * @param {Array<Block>}
+ * @returns {boolean}
+ */
+const isValid = (chain) => {
+    const genesisBlock = chain[0];
+    if (genesisBlock.hash != Block.computeHash(genesisBlock.index, genesisBlock.timestamp, genesisBlock.data, genesisBlock.previousHash))
+        return false;
+
+    for (let i = 1; i < chain.length; i++) {
+        if (!Block.isBlockValid(chain[i], chain[i - 1]))
+            return false
+    }
+
+    return true;
+}
+
+module.exports = chain, isValid;

b410c37ec9a53381358dc74eaaf449956e42e84b
Urban Prah <urban.prah@student.um.si>
2020-04-19 00:03:12 +0200

    Changed controller.ts to .js, updated function definitions

---
+++ blockchain/controller.js
@@ -0,0 +1,50 @@
+const Block = require("./Block.js");
+
+var chain = new Array(Block.genesisBlock);
+
+/**
+ * @description Functions to manipulate the blockchain
+ */
+module.exports = {
+    /**
+     * blockchain.get()
+     * @returns {Array<Block>} returns the entire blockchain
+     */
+    get: function () {
+        return blockchain;
+    },
+
+    /**
+     * blockchain.latestBlock()
+     * @returns {Block} returns the latest block
+     */
+    latestBlock: function () {
+        return Blockchain.chain[Blockchain.chain.length - 1];
+    },
+
+    /**
+     * blockchain.createBlock()
+     * @param {string} data
+     * @returns {Block} returns a new block
+     */
+    createBlock: function (data) {
+    },
+
+    /**
+     * blockchain.replace()
+     * @description checks length, validates the chain, sets blockchain to the best one
+     * @param {Array<Block>} candidateChain
+     * @returns {boolean} success
+     */
+    replace: function (candidateChain) {
+    },
+
+    /**
+     * blockchain.appendBlock()
+     * @description checks validity, appends Block
+     * @param {Block} candidateBlock
+     * @returns {boolean} success
+     */
+    appendBlock: function (candidateBlock) {
+    }
+}
